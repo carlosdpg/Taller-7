f89bb57f9208601b09b920d74cc7142ba4dbc75a
Carlos Daniel Parra <cdanielpg@outlook.com>
2021-03-04 23:02:45 -0500

    appjsUPD30

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ $(document).on('ready',function (){
 			$.each(data, function(id, dpto) {
 				$("#departamento").append('<option value="' + id + '">' + dpto.departamento + '</option>');
 				let ciudades = dpto.ciudades;
-				$("#ciudad").append('<option name="' + dpto.id + "" + id + '">' + ciudades[id] + '</option>');
 			});
 		});
 
 		$("#departamento").on("change", function(){
 			 let dptoid = $("#departamento").val();
-			 console.log(dptoid);
 			 $.getJSON('https://raw.githubusercontent.com/marcovega/colombia-json/master/colombia.min.json', function(data) {
 				 let cdd = data[dptoid].ciudades;
 				 console.log(cdd);
-				 
+				 $.each(cdd,function(id) {
+					 $("#ciudad").append('<option value="' + id + '">' + id + '</option>');
+				 });
 			 });
 		 });
 	});
